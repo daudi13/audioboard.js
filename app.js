d705327b2271538ce3b9c459a3bfa6ffe61af3fc
daudi13 <davouma13@gmail.com>
2021-05-28 04:17:57 +0300

    commit css

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ sounds.forEach(sound => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = sound;
+    
     document.getElementById('buttons').appendChild(btn)
 }) 
